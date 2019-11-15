e2ea5f4c40797653b720af6fea6bf928d8249456
马军伟 <majunwei@weidian.com>
2019-11-15 19:58:20 +0800

    初始化项目

---
+++ jbone-bpm-server/src/main/java/cn/jbone/bpm/JboneBpmServerBanner.java
@@ -0,0 +1,12 @@
+package cn.jbone.bpm;
+
+
+import cn.jbone.banner.AbastractJboneBanner;
+
+public class JboneBpmServerBanner extends AbastractJboneBanner {
+
+    @Override
+    protected String getTitle() {
+        return "Jbone BPM Server";
+    }
+}
